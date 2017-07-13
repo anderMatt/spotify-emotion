1f434d7ae87c5fbacf85cb29a4315fbe18e7da75
Matthew Anderson <matthew.anderson@DTG.local>
2017-07-13 15:53:03 -0400

    Add About text.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,10 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   template: `
-		<div class="jumbotron text-center">Upload an image to generate a Spotify Playlist</div>
+	<div class="jumbotron text-center">
+		<h3>Generate a Spotify playlist from a picture of your face!</h3>
+		Your facial expression is analyzed to detect which emotion is most prevalent: <span class="emotions"> Anger, Happiness, Saddness, or Neutral Expression. </span>
+		A sample Spotify playlist is generated from music genres that evoke the detected emotion.
+	</div>
   `,
-  // styleUrls: ['./about.component.css']
-  styles: [``]
+  styles: [`
+  	.jumbotron{
+  		font-size: 1.2em;
+  		text-align: center;
+  	}
+  	h3{
+  		margin-bottom: 15px;
+  	}
+  	.emotions{
+  		font-weight: bold;
+  		font-size: 1.1em;
+  	}
+  `]
 })
 export class AboutComponent implements OnInit {
 
