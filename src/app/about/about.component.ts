60dcab6fd2d9417d4df7b76dd1ce1049e6d318c2
Matthew Anderson <matthew.anderson@DTG.local>
2017-07-17 14:41:16 -0400

    Add font-type and 'about' background color.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,21 +5,34 @@ import { Component, OnInit } from '@angular/core';
   template: `
 	<div class="jumbotron text-center">
 		<h3>Generate a Spotify playlist from a picture of your face!</h3>
-		Your facial expression is analyzed to detect which emotion is most prevalent: <span class="emotions"> Anger, Happiness, Saddness, or Neutral Expression. </span>
-		A sample Spotify playlist is generated from music genres that evoke the detected emotion.
+    <p>
+    Facial expressions in your photo are analyzed to detect which of the following emotions is most prevalent: <span class="emotions"> Anger, Happiness, Sadness, or Neutral Expression. </span>
+		A sample Spotify playlist is then generated from music genres that evoke the detected emotion. Try it out!
+    </p>
 	</div>
   `,
   styles: [`
   	.jumbotron{
-  		font-size: 1.2em;
+  		font-size: 1.1em;
   		text-align: center;
+      background-color: #004080;
+      color: white;
   	}
-  	h3{
-  		margin-bottom: 15px;
+  	.jumbotron h3{
+  		margin-bottom: 25px;
+      font-size: 1.5em;
+      border-bottom: 1px solid white;
+      padding-left: 25px;
+      padding-right: 25px;
+      padding-bottom: 10px;
+      display: inline-block;
   	}
   	.emotions{
+      display: block;
   		font-weight: bold;
   		font-size: 1.1em;
+      margin-top: 15px;
+      margin-bottom: 15px;
   	}
   `]
 })
